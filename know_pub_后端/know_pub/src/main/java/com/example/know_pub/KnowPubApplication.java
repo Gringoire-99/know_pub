6271dc26cc1997578522239183081374b10d13f4
gringoire <qq15985760072@sina.com>
2023-05-16 17:45:21 +0800

    normal update

---
+++ know_pub_后端/know_pub/src/main/java/com/example/know_pub/KnowPubApplication.java
@@ -0,0 +1,13 @@
+package com.example.know_pub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowPubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowPubApplication.class, args);
+    }
+
+}
---
+++ know_pub_后端/know_pub/src/test/java/com/example/know_pub/KnowPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.know_pub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowPubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
